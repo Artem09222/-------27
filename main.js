aa9ccb9d251046990c722e13cfa19298973548d4
Artem <artemig589@gmail.com>
2023-06-29 15:58:32 +0300

    аівавівіа

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-let h1 = document.querySelector('.first')
-let spn = document.querySelector('.second')
+let spanClicks = document.querySelector('.first')
+let spanKeys = document.querySelector('.second')
 let button = document.querySelector('.button')
-let counterValue = 0;
-let keyCode = 0;
-window.addEventListener('mousedown', event => {
-    counterValue++;
-    h1.textContent = counterValue;
-})
-window.addEventListener('keydown', event => {
-    keyCode++;
-    spn.textContent = keyCode;
-})
-button.addEventListener('click', () => {
-   counterValue = 0 -1;
-})
 
+let enter = {
+    clicks: 0, 
+    keys: 0
+}
+ 
+let read = renderLocal();
+if (read != null){
+    enter = read;
+}
+startPosition()
 
+function startPosition(){
+   spanClicks.textContent = enter.clicks;
+   spanKeys.textContent = enter.keys;
+}
+
+function toDoLocal(){
+   let a = JSON.stringify(enter)
+   localStorage.setItem('objects', a);
+}
+
+function renderLocal(){
+    let b = localStorage.getItem('objects')
+    JSON.parse(b)
+    if (b == null){
+        return null;
+    }
+    return JSON.parse(b)
+}
+
+window.addEventListener('click', () => {
+    enter.clicks++ 
+    startPosition()
+    toDoLocal()
+})
+window.addEventListener('keydown', () => {
+    enter.keys++
+    startPosition()
+    toDoLocal()
+})
 
 // let input = document.querySelector('.forms-input')
 // let btn = document.querySelector('.forms-btn')
